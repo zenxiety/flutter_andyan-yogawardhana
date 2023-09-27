c3df3efb0a226cce7498ff3c68b22ac52d0344d4
zenxiety <andyanyoga22@gmail.com>
2023-09-27 19:07:26 +0700

    fix - move yes/no image dialog to separate popup

--- 12_ Assets Dialog Bottomsheet Navigation/praktikum/asset_dialog_bottomsheet_navigation/lib/components/bottomsheet.dart
+++ 12_ Assets Dialog Bottomsheet Navigation/praktikum/asset_dialog_bottomsheet_navigation/lib/components/bottomsheet.dart
@@ -5,56 +5,73 @@ Future buildBottomSheet(BuildContext context, String image) {
   return showModalBottomSheet(
     context: context,
     shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.vertical(top: Radius.circular(12))),
+      borderRadius: BorderRadius.vertical(
+        top: Radius.circular(12),
+      ),
+    ),
     barrierColor: Colors.black87,
-    isDismissible: false,
     builder: (context) => Container(
       margin: const EdgeInsets.all(20),
       height: 220,
       child: Column(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(12),
-            child: image.contains("http")
-                ? Image.network(
-                    image,
-                    height: 160,
-                    fit: BoxFit.cover,
-                  )
-                : Image.asset(
-                    image,
-                    height: 160,
-                    fit: BoxFit.cover,
-                  ),
+        children: <Widget>[
+          InkWell(
+            onTap: () {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: const Text(
+                      "Open Image?",
+                      textAlign: TextAlign.center,
+                    ),
+                    content: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        ElevatedButton(
+                          onPressed: () => Navigator.pop(context),
+                          child: Text(
+                            "No",
+                            style: GoogleFonts.poppins(fontSize: 14),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 20,
+                        ),
+                        ElevatedButton(
+                          onPressed: () => Navigator.pushReplacementNamed(
+                            context,
+                            '/image_detail',
+                            arguments: image,
+                          ),
+                          child: Text(
+                            "Yes",
+                            style: GoogleFonts.poppins(fontSize: 14),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              );
+            },
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: image.contains("http")
+                  ? Image.network(
+                      image,
+                      height: 160,
+                      fit: BoxFit.cover,
+                    )
+                  : Image.asset(
+                      image,
+                      height: 160,
+                      fit: BoxFit.cover,
+                    ),
+            ),
           ),
           const SizedBox(
             height: 12,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: () => Navigator.pop(context),
-                child: Text(
-                  "No",
-                  style: GoogleFonts.poppins(fontSize: 14),
-                ),
-              ),
-              const SizedBox(
-                width: 20,
-              ),
-              ElevatedButton(
-                onPressed: () => Navigator.pushNamed(
-                  context,
-                  '/image_detail',
-                  arguments: image,
-                ),
-                child: Text(
-                  "Yes",
-                  style: GoogleFonts.poppins(fontSize: 14),
-                ),
-              ),
-            ],
           ),
         ],
       ),
